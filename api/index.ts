42ecc33dad6fbc14530b8eff290b48c432b1a149
KonradDepowski <depowskikonrad@gmail.com>
2026-05-22 15:32:34 +0200

    feat: add CORS support

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import mainRouter from "../index";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
 
